13c50240a97f0952d6f5a2a1e6cad08dc2d88164
David Essien <davidessienshare@gmail.com>
2022-10-14 16:57:25 -0700

    print all numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry poing of program
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+
+	printf("\n");
+	return (0);
+}
+
