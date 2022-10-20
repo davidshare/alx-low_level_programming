d3fecea1b8950a849b2391397c2b44b7d248cb51
David Essien <davidessienshare@gmail.com>
2022-10-19 21:41:17 -0700

    calculate the sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - check the description
+ * Description: calculate the sum of multiples of 5 and 3
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
