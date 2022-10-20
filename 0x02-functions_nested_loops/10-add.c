a3e307d6c7728f7624ef9ef2ce2c65c4705f8754
David Essien <davidessienshare@gmail.com>
2022-10-19 21:18:37 -0700

    return the sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - check the description
+ * @a: first input number
+ * @b: second input number
+ * Description: function that returns the sum of two numbers
+ * Return: returns the absolute value
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
