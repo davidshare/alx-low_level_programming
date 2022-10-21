121b62ec393c9a3dec6af4b19d33aad7c60c6341
David Essien <davidessienshare@gmail.com>
2022-10-20 18:47:05 -0700

    add test for main file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that checks if a number is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+# 0x03. C - Debugging
+
+## Files and descriptions
+[0-main.c](./0-main.c) - File to test main.c file
+[main.h](./main.h) - File containing list of function declarations
